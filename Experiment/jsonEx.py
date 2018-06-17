67074447435eb67e9f3a7fa91da8040c2d07be60
nawnoes <nawnoes@gmail.com>
2018-06-17 21:22:10 +0900

    <Done> - Json file transformation Example test.
    <TO DO>
    - Playing self-play using Json file that is recorded about self-play game information.

---
+++ Experiment/jsonEx.py
@@ -0,0 +1,29 @@
+import json
+
+
+dict1 = { 'name' : 'song', 'age' : 10 }
+j1 = {"name":"홍길동", "birth":"0525", "age": 30}
+json_string = json.dumps(j1)
+
+#json 파일 저장
+with open('../Data/Sample.json',mode='w') as f:
+    json.dump(json_string,f,indent=2)
+
+#json 파일 읽어오기
+with open('../Data/Sample.json', mode='r') as f:
+    json_string = json.load(f)
+
+print(dict1['name'])
+print(dict1['age'])
+
+dicToJson = json.dumps(dict1)
+print(dicToJson)
+print(type(dicToJson))
+print(json_string)
+
+print("from json to dictionary")
+
+dict2 = json.loads(dicToJson)
+
+print(dict2)
+print(type(dict2))
